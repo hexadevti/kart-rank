b37301fa965fc3dad409a516d4e40b8c227da093
hexadevti <lucianobf@uol.com.br>
2017-03-25 16:00:08 -0300

    Falha do commit

---
+++ kartrankUnitTest/RaceResultTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using kartrank.BusinessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace kartrankUnitTest
+{
+    [TestClass]
+    public class RaceProcessTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RaceProcess_Blank_File_Name()
+        {
+            RaceProcess rp = new RaceProcess(null);
+        }
+
+        [TestMethod]
+        public void RaceProcess_Internal_file()
+        {
+            RaceProcess rp = new RaceProcess();
+            var raceResults = rp.RaceResult();
+
+            Assert.IsTrue(raceResults.Any());
+        }
+    }
+}
